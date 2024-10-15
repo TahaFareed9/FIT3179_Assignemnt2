4041cabeae513f0a0adca5259a2eb01a290d647b
TahaFareed9 <144083427+TahaFareed9@users.noreply.github.com>
2024-10-15 18:05:34 +0800

    Added the Assignment 2 files

---
+++ Assignment2.js
@@ -0,0 +1,55 @@
+var vg_1 = "MAP.vg.json";
+
+vegaEmbed("#map", vg_1)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_2 = "TRELLIS.vg.json";
+
+vegaEmbed("#trellis", vg_2)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_3 = "RADIAL_JOHOR.vg.json";
+
+vegaEmbed("#radialjohor", vg_3)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_4 = "RADIAL_KLANG.vg.json";
+
+vegaEmbed("#radialklang", vg_4)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_5 = "RADIAL_MELAKA.vg.json";
+
+vegaEmbed("#radialmelaka", vg_5)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_6 = "RADIAL_KEDAH.vg.json";
+
+vegaEmbed("#radialkedah", vg_6)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_7 = "RADIAL_PULAU.vg.json";
+
+vegaEmbed("#radialpulau", vg_7)
+  .then(function (result) {
+  })
+  .catch(console.error);
+
+var vg_8 = "RADIAL_SARAWAK.vg.json";
+
+vegaEmbed("#radialsarawak", vg_8)
+  .then(function (result) {
+  })
+  .catch(console.error);
